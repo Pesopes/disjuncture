f3557faececae522b7258d256dbb224719bd0ed9
Pesopes <jeniktelefon@gmail.com>
2025-07-19 12:14:57 +0200

    prepend base path

--- src/lib/DashboardApps/FileExplorer.svelte
+++ src/lib/DashboardApps/FileExplorer.svelte
@@ -13,6 +13,10 @@
   export let title = "New window"
   export let isPinned = false
 
+  // Set this to your deployment subdirectory, or leave as '' for root
+// @ts-ignore
+  const basePath = window.__BASE_PATH__ || '';
+
 
 	// console.log(import("./../Popup.svelte?raw"))
 	//assign click function to all files (uses recursion)
@@ -46,24 +50,24 @@
 	let codeViewers = []
 	async function openCode(codePath){
 		// const file = await import(codePath+"?raw")
-		const githubPath = "https://raw.githubusercontent.com/Pesopes/disjuncture/master/src/"+codePath
+		const githubPath = basePath + "/" + "https://raw.githubusercontent.com/Pesopes/disjuncture/master/src/" + codePath;
 		const file = await fetch(githubPath)
 		const content = await file.blob()
 
 		//if image open image viewer instead
 		if(imageExtensions.includes(codePath.slice(codePath.lastIndexOf('.') + 1))){
-			openImage(githubPath,codePath.slice(codePath.lastIndexOf('/') + 1))
+			openImage(basePath + "/" + githubPath,codePath.slice(codePath.lastIndexOf('/') + 1))
 			return
 		}
 		//if sound open sound viewer instead
 		if(soundExtensions.includes(codePath.slice(codePath.lastIndexOf('.') + 1))){
-			openSound(githubPath,codePath.slice(codePath.lastIndexOf('/') + 1))
+			openSound(basePath + "/" + githubPath,codePath.slice(codePath.lastIndexOf('/') + 1))
 			return
 		}
 		codeViewers = [...codeViewers,
 			{
 				src:content,
-				srcPath:githubPath,
+				srcPath:basePath + "/" + githubPath,
 				type:"fileType",
 				visible:true,
 				title:codePath,
@@ -85,7 +89,7 @@
 	function openImage(imageSrc,imageName){
 		imageViewers = [...imageViewers,
 			{
-				source:imageSrc,
+				source:basePath + "/" + imageSrc,
 				title:imageName,
 				visible:true
 		}]
@@ -95,7 +99,7 @@
 	function openSound(soundSrc,soundName){
 		soundViewers = [...soundViewers,
 			{
-				source:soundSrc,
+				source:basePath + "/" + soundSrc,
 				title:soundName,
 				visible:true
 		}]
